319fd03395b1c65f76131838b40fe2328905fdc5
sammyok <sammyok@163.com>
2020-05-25 23:31:00 +0800

    kubectl

---
+++ kubectl/kubectl_install.sh
@@ -0,0 +1,9 @@
+#install kubectl and setup autocompletion
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+apt install bash-completioni
+source /usr/share/bash-completion/bash_completion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+kubectl completion bash >/etc/bash_completion.d/kubectl
