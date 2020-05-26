462ddd4ee58e258bcd36cb6f113379af60cc6cc2
sammyok <sammyok@163.com>
2020-05-26 21:49:22 +0800

    kubeadm

--- kubeadm/kubeadm_install.sh
+++ kubeadm/kubeadm_install.sh
@@ -1,8 +1,7 @@
-
+#install kubectl kubelet kubeadm
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
-
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
-
-apt-get update
+apt update
+apt install -y kubectl kubelet kubeadm
